c416c180d313ab4b602ebdcf2d51ef55384f736f
tidesjunior2018 <aristidesadj@gmail.com>
2020-12-01 17:13:38 -0300

    Update ex046.py

--- download codigo fontes/PythonExercicios/ex046.py
+++ download codigo fontes/PythonExercicios/ex046.py
@@ -1,3 +1,7 @@
+'''
+46-Faça um programa que mostre na tela uma contagem regressiva para estouro
+dos fogos de artificios ,indo de 10 a 0 , com uma pausa de 1 segundo entre eles.
+'''
 import emoji
 from time import sleep
 for c in range(10,-1,-1):
