0b88ec0f3cb97dde42d34f38534fd20cc3b4e2ec
tidesjunior2018 <aristidesadj@gmail.com>
2020-12-10 17:16:13 -0300

    Update ex048.py

--- download codigo fontes/PythonExercicios/ex048.py
+++ download codigo fontes/PythonExercicios/ex048.py
@@ -1,3 +1,7 @@
+'''
+48-Faça um programa que calcule a soma de todos os números impares que são
+múltiplos de três e que se encontram no intervalo de 1 até 500
+'''
 soma=0
 cont=0
 for c in range(1,500,2):
