e1e90fff45bfd44e82ee53b13c4324d2dcecdf41
tidesjunior2018 <aristidesadj@gmail.com>
2020-12-10 17:40:59 -0300

    Update ex050.py

--- download codigo fontes/PythonExercicios/ex050.py
+++ download codigo fontes/PythonExercicios/ex050.py
@@ -1,3 +1,7 @@
+'''
+50-Desenvolva um programa que leia seis números inteiros e mostre a soma
+apenas daqueles que forem pares.Se o valor digitado for impar,desconsidere-o.
+'''
 soma=0
 cont=0
 for c in range(1,7):
