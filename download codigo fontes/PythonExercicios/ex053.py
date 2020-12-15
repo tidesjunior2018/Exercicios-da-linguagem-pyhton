59815adfd6f181a3eb546e2417c8a74682d56238
tidesjunior2018 <aristidesadj@gmail.com>
2020-12-15 18:08:33 -0300

    Update ex053.py

--- download codigo fontes/PythonExercicios/ex053.py
+++ download codigo fontes/PythonExercicios/ex053.py
@@ -1,3 +1,12 @@
+'''
+53-Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo desconsiderando os espaços.
+
+EX: APOS A SOPA
+    A SACADA DA CASA
+    A TORRE DA DERROTA
+    O LOBO AMA O BOLO
+    ANOTARAM A DATA DA MARATONA
+'''
 frase=str(input('Digite uma frase: ')).strip().upper()
 palavras=frase.split()
 junto=''.join(palavras)
