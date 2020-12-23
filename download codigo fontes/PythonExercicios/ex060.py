c1a9564d1f25e9e3647b4e4abc84a6397d1aad83
tidesjunior2018 <aristidesadj@gmail.com>
2020-12-23 18:19:07 -0300

    Update ex060.py

--- download codigo fontes/PythonExercicios/ex060.py
+++ download codigo fontes/PythonExercicios/ex060.py
@@ -1,3 +1,12 @@
+'''
+60-Faça um programa que leia um numero qualquer e mostre seu fatorial.
+
+EX:
+
+5!= 5 X 4 X 3 X 2 X 1 = 120
+'''
+
+
 '''fat=1
 n=int(input('Digite um valor: '))
 for c in range(n,0,-1):
