b9a6ab1f9b3105261776a87410eb2f82625504e0
tidesjunior2018 <aristidesadj@gmail.com>
2021-01-06 17:59:58 -0300

    Update ex063.py

--- download codigo fontes/PythonExercicios/ex063.py
+++ download codigo fontes/PythonExercicios/ex063.py
@@ -1,3 +1,9 @@
+'''
+63-Escreva um programa que leia um número n inteiro qualquer e mostre na tela os n primeiros elementos de uma sequência de fibonacci. 
+
+Ex:
+0->1->1->2->3->5->8
+'''
 print('='*30)
 print('Sequencia de fibonnaci')
 print('='*30)
