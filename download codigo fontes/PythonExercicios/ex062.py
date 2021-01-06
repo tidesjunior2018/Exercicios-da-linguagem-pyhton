eb21114ba38f12afffb18e7666348b7136adb728
tidesjunior2018 <aristidesadj@gmail.com>
2021-01-06 17:45:29 -0300

    Update ex062.py

--- download codigo fontes/PythonExercicios/ex062.py
+++ download codigo fontes/PythonExercicios/ex062.py
@@ -1,3 +1,6 @@
+'''
+62-Melhore o desafio 061,perguntando para o usuário se ele quer mostrar mais alguns termos.O programa encerrará quando ele disser que quer mostrar 0 termos.
+'''
 primeirotermo=int(input('Digite o primeiro termo: '))
 razao=int(input('Digite a razao: '))
 i=1
