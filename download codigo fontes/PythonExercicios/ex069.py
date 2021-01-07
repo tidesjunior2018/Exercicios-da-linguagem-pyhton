32d27321b974aea75b837c176b11789908ae5806
tidesjunior2018 <aristidesadj@gmail.com>
2021-01-07 18:02:26 -0300

    Update ex069.py

--- download codigo fontes/PythonExercicios/ex069.py
+++ download codigo fontes/PythonExercicios/ex069.py
@@ -1,3 +1,10 @@
+'''
+69-Crie um programa que leia a idade e o sexo de várias pessoas.A cada pessoa casdatrada,o programa deverá perguntar se o usuário quer continuar ou não.No final,mostre:
+
+A)Quantas pessoas tem mais de 18 anos.
+B)Quantos homens foram cadastrados.
+C)Quantas mulheres tem menos de 20 anos.
+'''
 contidade = 0
 conthomens = 0
 contmulher = 0
