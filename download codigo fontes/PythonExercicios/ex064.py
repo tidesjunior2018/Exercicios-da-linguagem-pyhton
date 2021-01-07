af7a1f57b0ed35e7391c043115821e0341f3038f
tidesjunior2018 <aristidesadj@gmail.com>
2021-01-07 17:05:10 -0300

    Update ex064.py

--- download codigo fontes/PythonExercicios/ex064.py
+++ download codigo fontes/PythonExercicios/ex064.py
@@ -1,3 +1,6 @@
+'''
+64-Crie um programa que leia varios números inteiros pelo teclado.O programa só vai parar quando o usuário digitar 999,que é a condição de parada.No final,mostre quantos números foram digitados e qual foi a soma (desconsiderando o flag).
+'''
 soma=0
 n=999
 flag=False
