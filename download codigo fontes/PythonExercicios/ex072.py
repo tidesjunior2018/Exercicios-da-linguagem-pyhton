0dcc1edbd397df9d791c8d4ee26c6db82699c050
tidesjunior2018 <aristidesadj@gmail.com>
2021-01-13 15:36:02 -0300

    Update ex072.py

--- download codigo fontes/PythonExercicios/ex072.py
+++ download codigo fontes/PythonExercicios/ex072.py
@@ -1,3 +1,6 @@
+'''
+72-Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso,de zero até vinte.Seu programa deverá ler um número pelo teclado (entre zero e vinte) e mostrá-lo por extenso . 
+'''
 lista=('zero','um','dois','tres',
      'quatro','cinco','seis','sete',
      'oito','nove','dez','onze','doze',
