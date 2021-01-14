61c43e0e78f6d4576034a8851ef16d834180711c
tidesjunior2018 <aristidesadj@gmail.com>
2021-01-14 16:22:04 -0300

    Update ex077.py

--- download codigo fontes/PythonExercicios/ex077.py
+++ download codigo fontes/PythonExercicios/ex077.py
@@ -1,3 +1,6 @@
+'''
+77-Crie um programa que tenha uma tupla com várias palavras(não usar acentos).Depois disso,você deve mostrar,para cada palavra quais são suas vogais.
+'''
 palavras=('AMIGO','BOLA','ESCOLA','FACULDADE','RELOGIO')
 for p in palavras:
     print(f'\n\033[37m Na palavra {p} temos :',end=' ')
