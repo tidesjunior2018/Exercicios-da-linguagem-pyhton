fd924bbc5f896c357d00a8aea74f11a359361a2d
tidesjunior2018 <aristidesadj@gmail.com>
2021-03-24 19:26:29 -0300

    Update ex088.py

--- download codigo fontes/PythonExercicios/ex088.py
+++ download codigo fontes/PythonExercicios/ex088.py
@@ -1,14 +1,32 @@
 '''
 88-Faça um programa que ajuda um jogador da MEGASENA a criar PAlPITES.O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números de 1 a 60 para cada jogo cadastrando em uma lista completa.
 '''
-import random
-numerosaleatorios=[]
+from random import randint
+numeros=[]
+jogos=[]
+print('-'*30)
+print('    Sorteio da Mega Sena    ')
+print('-'*30)
+numerosdejogos=int(input('Quantos jogos você quer que eu sorteie:'))
+total=0
+while total <= numerosdejogos-1:
+    cont=0
+    while True:
+        num=randint(1,60)
+        if num not in numeros:
+            numeros.append(num)
+            cont+=1
+        if cont >= 6:
+            break
+    numeros.sort()
+    jogos.append(numeros[:])
+    numeros.clear()
+    total+=1
+print()
+print('>'*30)
+print(f' Sorteando {numerosdejogos} jogos ')
+print('>'*30)
 
-numerosjogos=int(input('Quer que eu sorteie quantos jogos:'))
-
-for i in range(numerosjogos):
-    numerosaleatorios=[]
-    for j in range(6):
-        numerosaleatorios.append(random.randint(1,61))
-    print(f'Jogo {i}:{numerosaleatorios}')
-    print()
+for i,n in enumerate(jogos):
+    print(f'Jogo {i+1}: {n}')
+print()
