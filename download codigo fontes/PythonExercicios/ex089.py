f7e6d3e8d754710ff8953edd2a13918693297d76
tidesjunior2018 <aristidesadj@gmail.com>
2021-04-14 19:43:34 -0300

    Update ex089.py

--- download codigo fontes/PythonExercicios/ex089.py
+++ download codigo fontes/PythonExercicios/ex089.py
@@ -8,13 +8,24 @@ continuar = 'S'
 
 while continuar != 'n':
    nome = str(input(f'Entre com o nome do aluno: '))
-   nota1 = int(input(f'Entre com a nota 1 do aluno {nome}: '))
-   nota2 = int(input(f'Entra com a nota 2 do aluno {nome}: '))
+   nota1 = float(input(f'Entre com a nota 1 do aluno {nome}: '))
+   nota2 = float(input(f'Entra com a nota 2 do aluno {nome}: '))
    listaAlunos.append([nome, nota1, nota2])
    qtdAlunos += 1
    continuar = str(input('Quer continuar? [S/n]').lower())
   # continuar = continuar.lower()
 
+print(f'{"N°":<4}{"NOME:":<10}{"MEDIA:":>8}')
 for i in listaAlunos:
-  media = i[1]+i[2]/2
-  print(listaAlunos.index(i),i[0],media)
+  media = (i[1]+i[2])/2
+  print(f'{listaAlunos.index(i):<4}{i[0]:<10}{media:>8.1f}')
+
+while True:
+    opc=int(input('Você que ver as notas de que aluno: (999 para interromper)'))
+    if opc == 999:
+        print('Finalizando...')
+        break
+    if opc < len(listaAlunos):
+        print(f'Notas de {listaAlunos[opc][0]} são {listaAlunos[opc][1]} e '
+              f'{listaAlunos[opc][2]}')
+print('<<<','Volte Sempre!!!','<<<')
