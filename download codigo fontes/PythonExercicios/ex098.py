92b7bb14695581c9fd4b65d1322eb8bf0ca9c6fb
tidesjunior2018 <aristidesadj@gmail.com>
2021-09-03 19:02:49 -0300

    Update ex098.py

--- download codigo fontes/PythonExercicios/ex098.py
+++ download codigo fontes/PythonExercicios/ex098.py
@@ -13,16 +13,28 @@ from time import sleep
 
 def contador(inicio, fim, passo):
 
-    if inicio > fim:
-        passo *= -1
-    elif inicio > fim and passo < 0:
-        passo *= 1
-    elif inicio < fim and passo < 0:
-        passo *= -1
+    if passo == 0:
+        passo = 1
 
-    for cont in range(inicio, fim, passo):
-        print(f'{cont}', end=' ', flush=True)
-        sleep(0.5)
+    if inicio < fim:
+        if passo < 0:
+            passo = abs(passo)
+        escreva(f'Contagem de {inicio} até {fim} de {passo} em {passo}')
+        cont = inicio
+        for cont in range(inicio, fim, passo):
+            print(f'{cont}', end=' ', flush=True)
+            sleep(0.5)
+        print('Fim!!!')
+
+    if inicio > fim:
+        if passo > 0:
+            passo *= -1
+        escreva(f'Contagem de {inicio} até {fim} de {abs(passo)} em {abs(passo)}')
+        cont = inicio
+        for cont in range(inicio, fim, passo):
+            print(f'{cont}', end=' ', flush=True)
+            sleep(0.5)
+        print('Fim!!!')
     print()
 
 
@@ -34,15 +46,12 @@ def escreva(msg):
 
 
 #A)
-escreva('Contagem de 1 até 10, de 1 em 1')
-contador(1, 11, 1)
+contador(1, 10, 1)
 #B)
-escreva('Contagem de 10 até 0, de 2 em 2')
-contador(10, -1, 2)
+contador(10, -1, -2)
 #C)
 escreva('Agora é a sua vez de personalizar')
 inicio = int(input('Inicio: '))
 fim = int(input('Fim: '))
 passo = int(input('Passo: '))
-escreva(f'Contagem de {inicio} até o {fim} de {passo} em {passo}')
 contador(inicio, fim, passo)
